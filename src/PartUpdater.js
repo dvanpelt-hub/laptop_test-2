e03658a29bc30ccd2dcf3fa31114c28abe7decec
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2020-11-16 19:24:26 -0500

    PartUpdater.js and TotalCost.js finished

--- src/PartUpdater.js
+++ src/PartUpdater.js
@@ -5,7 +5,7 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
   currency: 'USD'
 });
 
-class UserCart extends React.Component{
+class PartUpdater extends React.Component{
 
   state = { selected: {} };
 
@@ -24,22 +24,11 @@ class UserCart extends React.Component{
         </div>
       );
     });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
       
       return(
         <section className="main__summary">
           <h2>Your cart</h2>
             {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
         </section>
       )
   }
