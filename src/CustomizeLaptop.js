9f6ac40488758906a0785845b24e2e9036d027d7
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2020-11-16 19:40:13 -0500

    Final working v1

--- src/CustomizeLaptop.js
+++ src/CustomizeLaptop.js
@@ -1,75 +1,17 @@
 import React from 'react';
-//import CustomizeLaptop from 'CustomizeLaptop';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-import './App.css';
-
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
-
+import OptionSelector from './OptionSelector';
 
 class CustomizeLaptop extends React.Component{
 
   state = { selected: null };
 
-  selected = () => {
-      this.setState(
-        prevState => ({ selected: prevState.selected })
-      )
-    };
-
-  updateFeature = (feature, newValue) => {
-    const selected = Object.assign({}, this.state.selected);
-    selected[feature] = newValue;
-    this.setState({
-      selected
-    });
-  };
-
-
   render() {
-
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={this.state.selected != null && item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
 
     return(
           <form className="main__form">
             <h2>Customize your laptop</h2>
-            {features}
+            <OptionSelector features={ this.props.features } />
           </form>
-
     )
   }
 }
--- src/OptionSelector.js
+++ src/OptionSelector.js
@@ -1,12 +1,34 @@
 import React from 'react';
+//import CustomizeLaptop from 'CustomizeLaptop';
+
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
 import slugify from 'slugify';
+import './App.css';
 
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD'
 });
 
+
 class OptionSelector extends React.Component{
+
+  state = { selected: null };
+
+  selected = () => {
+      this.setState(
+        prevState => ({ selected: prevState.selected })
+      )
+    };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
 
   render() {
 
@@ -21,7 +43,7 @@ class OptionSelector extends React.Component{
               id={itemHash}
               className="feature__option"
               name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
+              checked={this.state.selected != null && item.name === this.state.selected[feature].name}
               onChange={e => this.updateFeature(feature, item)}
             />
             <label htmlFor={itemHash} className="feature__label">
@@ -30,19 +52,24 @@ class OptionSelector extends React.Component{
           </div>
         );
       });
-    })
+
+      return (
+        <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+            <h3>{feature}</h3>
+          </legend>
+          {options}
+        </fieldset>
+      );
+    });
+
     return(
-        <div>
-          {features}
-          <fieldset className="feature" key={featureHash}>
-            <legend className="feature__name">
-              <h3>{feature}</h3>
-            </legend>
-              {options}
-          </fieldset>
-        </div>
-      )
-  };
-};
+      <div>
+        {features}
+      </div>
+    )
+  }
+}
+
 
 export default OptionSelector;
