0c0a82dad2cd4dafc807ca2ae93884a43b440330
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2020-11-19 15:53:42 -0500

    working

--- src/TotalCost.js
+++ src/TotalCost.js
@@ -9,8 +9,8 @@ class TotalCost extends React.Component{
 
   render() {
 
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
+    const total = Object.keys(this.props.selected).reduce(
+      (acc, curr) => acc + this.props.selected[curr].cost,
       0
     );
       
